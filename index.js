f56a9379632f5f6a529e9c4f37d80ebfd093ffe0
mikaela echo <mikaelaaecho@gmail.com>
2023-09-13 15:08:31 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email
+    };
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        const newId = data.id;
+        document.body.innerHTML += `<p>New ID: ${newId}</p>`;
+      })
+      .catch(error => {
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+      });
+  }
+  
